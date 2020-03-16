73b36ebe4f07cf534936862615c1351dbcfef7ff
gochicus <gochicus@gmail.com>
2020-03-17 03:49:15 +0900

    styles finished

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 const products = [
-    {id: 1, title: 'Notebook', price: 20000},
-    {id: 2, title: 'Mouse', price: 1500},
-    {id: 3, title: 'Keyboard', price: 5000},
-    {id: 4, title: 'Gamepad', price: 4500},
+    { id: 1, title: 'Notebook', price: 20000 },
+    { id: 2, title: 'Mouse', price: 1500 },
+    { id: 3, title: 'Keyboard', price: 5000 },
+    { id: 4, title: 'Gamepad', price: 4500 },
 ];
 
-const renderProduct = (title, price) => {
+const renderProduct = (title = "Product", price = 100) => {
     return `<div class="product-item">
                 <h3>${title}</h3>
                 <p>${price}</p>
@@ -15,7 +15,7 @@ const renderProduct = (title, price) => {
 
 const renderProducts = list => {
     const productList = list.map(item => renderProduct(item.title, item.price));
-    document.querySelector('.products').innerHTML = productList;
+    document.querySelector('.products').innerHTML = productList.join(' ');
 };
 
 renderProducts(products);
